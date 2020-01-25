f84650450b8aac5c12fd0abb856cba9e2a85b23c
Amit Agrahari <amit.agrahari@nagarro.com>
2020-01-25 15:02:46 +0530

    Module Basics

---
+++ Main.py
@@ -0,0 +1,18 @@
+from Calc import *
+
+print("When we want to use any module, we need to import module in our file")
+a = 6
+b = 2
+
+result = add(a,b)
+print("add : ",result)
+
+result = subtract(a,b)
+print("subtract : ",result)
+
+result = multiply(a,b)
+print("multiply : ",result)
+
+result = divide(a,b)
+print("divide : ",result)
+
