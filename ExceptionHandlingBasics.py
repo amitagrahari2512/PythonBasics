f6ade446618c6f39637e9d9644d8b0420cdc5318
Amit Agrahari <amit.agrahari@nagarro.com>
2020-02-01 12:03:50 +0530

    Exception Handling

--- ExceptionHandlingBasics.py
+++ ExceptionHandlingBasics.py
@@ -6,12 +6,38 @@ print("We can use try , except and finally for this")
 print("We can use exception overloading , with using multiple kind of errors")
 print("We can use errors in any order")
 print("We can use "
-      "1 - try,catch,finally"
-      "2 - try,finally"
-      "3 - try , catch"
-      "4 - But we can't use try alone")
+      "1 - try,except,finally sequence"
+      "2 - try,finally sequence"
+      "3 - try , except sequence"
+      "4 - But we can't use try alone"
+      "5 - And we can not use try,finally and except in this sequence")
 
 print("\n----------------------------------")
+
+a = 5
+b = 0
+try:
+    print("Resource open")
+    div = a/b
+    print("Div : ",div)
+    val = int(input("Enter a number"))
+    print("Val : ",val)
+
+except ZeroDivisionError as e:
+    print("No can not divide by zero :", e)
+except ValueError as e:
+    print("Value Error  : ",e)
+except Exception as e:
+    print("Generic Error : ",e)
+
+finally:
+    print("Resource close")
+
+
+print("\n----------------------------------")
+
+
+print("\n-----------------In Any Order Exception-----------------")
 
 a = 5
 b = 0
@@ -33,10 +59,24 @@ finally:
     print("Resource close")
 
 
-print("\n----------------------------------")
+print("\n-----------------In Any Order Exception-----------------")
+
+print("\n-----------------Try Finally Block-----------------")
 
 try:
     print("try")
 finally:
     print("Finally")
+
+print("\n-----------------Try Finally Block-----------------")
+
+print("\n--------------We can not use try,finally and except order")
+try:
+    print("try")
+finally:
+    print("finally")
+#except Exception e:
+ #   print("Exception")
+print("\n--------------We can not use try,finally and except order")
+
 
