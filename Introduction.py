363e6866c26807d54e9f9acc12a9dd371fe1e010
Amit Agrahari <amit.agrahari@nagarro.com>
2020-02-02 13:06:52 +0530

    Introduction Of python

---
+++ Introduction.py
@@ -0,0 +1,11 @@
+print("Python"
+      "1 - Python is a compiled as well as iterpreted language"
+      "2 - PythonCode --> compiled --> ByteCode --->Interpreter(Python Virtual Machine) ---> Machine Language"
+      "3 - Python is a set of Instructions : Their are many implementation is in market"
+      "3.1 - cPython - Written in c"
+      "3.2 - Jython - Written in java"
+      "3.3 - IronPython - written in .net"
+      "3.4 - PyPy -")
+
+print("We can run python on REPl using command Python fileName")
+print("We can run pythin in multiple IDE's like Pycharm and all.")
