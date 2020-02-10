fb49df4c8c6307d2726696729bc65de936c726f6
Amit Agrahari <amit.agrahari@nagarro.com>
2020-02-10 19:53:29 +0530

    DictionaryBasics

---
+++ DictionaryBasics.py
@@ -0,0 +1,8 @@
+print("maps or list")
+d = {"Amit":"onePlus","Himanshu":"Moto","AmitB":"MI"}
+print(d)
+print("type : ", type(d))
+print("keys in dictionary : ", d.keys())
+print("values in dictionary ; ", d.values())
+print("Get data from [key] : ",d["Amit"])
+print("Get data from get(key) method : ", d.get("Amit"))
