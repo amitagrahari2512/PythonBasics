5f6669ac2b043768aa735463753b46c4b09bd52d
Amit Agrahari <amit.agrahari@nagarro.com>
2020-05-24 00:20:37 +0530

    Dictionary Basics

--- DictionaryBasics.py
+++ DictionaryBasics.py
@@ -6,3 +6,11 @@ print("keys in dictionary : ", d.keys())
 print("values in dictionary ; ", d.values())
 print("Get data from [key] : ",d["Amit"])
 print("Get data from get(key) method : ", d.get("Amit"))
+
+print("Irerate Dictionary")
+
+for i in d:
+    print("Key : ",i)
+
+for i,j in d.items():
+    print("Key : {}, value : {}".format(i,j))
